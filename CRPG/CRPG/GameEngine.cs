b4c56e96233a8fed3a4a58f847da563708977894
1621372 <1621372@SC-VDGD-2QTKT52.local>
2019-12-10 09:42:23 -0800

    Update CRPG.csproj, GameEngine.cs, and 2 more files...

---
+++ CRPG/CRPG/GameEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRPG
+{
+    public static class GameEngine
+    {
+        public static string Version = "0.0.1";
+
+        public static void Initialize()
+        { //tells us the standard info on game, such as version
+            Console.WriteLine("Initializing Game Engine...");  
+            Console.WriteLine("This is verion {0}", Version);
+
+        }
+    }
+}
--- CRPG/CRPG/Program.cs
+++ CRPG/CRPG/Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Zach A. Clapper, 2019-2020
 namespace CRPG
 {
     class Program
     {
+        private static Player _player = new Player();
         static void Main(string[] args)
         {
             Console.WriteLine("I'm Zach");
+            GameEngine.Initialize();
+            _player.Name = "Zach";
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
