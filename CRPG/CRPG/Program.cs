143998639d3324c104f0c6ac28c49f826fc6d606
1621372 <1621372@SC-VDGD-2QTKT52.local>
2019-12-10 09:43:04 -0800

    Update Program.cs

---
+++ CRPG/CRPG/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Zach A. Clapper, 2019-2020 /
+namespace CRPG
+{
+    class Program
+    {
+        private static Player _player = new Player();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm Zach");
+            GameEngine.Initialize();
+            _player.Name = "Zach";
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
